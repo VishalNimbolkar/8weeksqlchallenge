26818f3ca66ab458fef90a24512eba71f823caad
Vishal Nimbolkar <80448632+VishalNimbolkar@users.noreply.github.com>
2023-11-15 12:56:33 +0530

    Que_01.sql

--- Danny's Diner /Que_01.sql
+++ Danny's Diner /Que_01.sql
@@ -1,1 +1,7 @@
- 
+-- 1. What is the total amount each customer spent at the restaurant?
+select s.customer_id,sum(mp.price) as total_amount 
+from sales s
+inner join menu mp 
+on s.product_id = mp.product_id
+group by s.customer_id
+order by s.customer_id; 
