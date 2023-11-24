d6081404916a5ade64ca2cb58ee36b5f0c4e1aef
Vishal Nimbolkar <80448632+VishalNimbolkar@users.noreply.github.com>
2023-11-24 17:38:50 +0530

    Create bonus.sql

---
+++ Pizza Runner/E - Bonus Question/bonus.sql
@@ -0,0 +1,14 @@
+INSERT INTO pizza_runner.pizza_names
+(pizza_id,pizza_name)
+VALUES(3,'Supreme');
+
+INSERT INTO pizza_runner.pizza_recipes
+VALUES
+(3,1),
+(3,5),
+(3,4),
+(3,3),
+(3,11),
+(3,8),
+(3,9),
+(3,6);
